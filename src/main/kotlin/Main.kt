0195f1ca1c120cdcc21647302238979175837358
Arturo Velázquez <A01018322@itesm.mx>
2018-02-19 11:34:07 -0600

    Basic setup for Bulma and Angular

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,30 @@
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import org.apache.log4j.BasicConfigurator
+import spark.Service
+import spark.Spark
+import spark.ModelAndView
+import spark.template.freemarker.FreeMarkerEngine
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.util.HashMap
+
+
+fun main(args: Array<String>) {
+    BasicConfigurator.configure()
+
+    Service.ignite().apply {
+        Spark.port(8050)
+        Spark.staticFileLocation("/public")
+
+        //Intializing the path
+        Spark.get("/", { _, _ ->
+            val model = HashMap<String, Any>()
+            model.put("message", "Hello Freemarker!")
+            ModelAndView(model, "hello.ftl") // located in src/test/resources/spark/template/freemarker
+        }, FreeMarkerEngine())
+
+
+
+    }
+}
---
+++ src/main/resources/spark/template/freemarker/hello.ftl
@@ -0,0 +1,16 @@
+<head>
+    <link rel="stylesheet" href="css/bulma/bulma.css">
+    <script src="javascript/angular/angular.min.js"></script>
+
+</head>
+<div>
+    <h1>
+        ${message}
+    </h1>
+    <span>
+
+    </span>
+    <input>
+
+    </input>
+</div>
