fba2acd46c7bcc8eec7a6bf077bb5b542ee70b44
Unknown <a01375051@itesm.mx>
2018-04-24 02:34:20 -0500

    Revert "Pantalla Effort Multipliers"
    This reverts commit 98a3ccd1a40c57abfc987168f93ea89eaa719915.

--- src/main/kotlin/com/tipa/Controller/Project/ProjectRenderController.kt
+++ src/main/kotlin/com/tipa/Controller/Project/ProjectRenderController.kt
@@ -1,8 +1,10 @@
 package com.tipa.Controller.Project
 
 import com.tipa.configuration
+import org.omg.CORBA.Object
 import spark.Request
 import spark.Response
+import spark.Spark
 import java.io.StringWriter
 import java.util.HashMap
 
@@ -11,18 +13,11 @@ class ProjectRenderController {
 
         fun renderScaleFactorsView(req:Request,resp:Response):String{
             val writer = StringWriter()
-            val formTemplate = configuration.getTemplate("templates/PojectValues/scaleFactors.ftl")
+            val formTemplate = configuration.getTemplate("templates/ScaleFactors/scaleFactors.ftl")
             val hasshecito = HashMap<String,Any>()
             hasshecito.put("message","HOLA MARINA2!!!!!!!")
             formTemplate.process( hasshecito, writer)
             return writer.toString();
-        }
-
-        fun renderEffortMultipliers(req:Request,resp:Response):String{
-            val writer = StringWriter()
-            val formTemplate = configuration.getTemplate("templates/PojectValues/effortMultipliers.ftl")
-            formTemplate.process(null,writer)
-            return writer.toString()
         }
     }
 }
